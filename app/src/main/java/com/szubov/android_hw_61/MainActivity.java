7b473b4ac5855a06982fea57338dcb90212c6bc2
Sergey Zubov <serjio.zubov@gmail.com>
2020-07-02 23:25:29 +0300

    for check

--- app/src/main/java/com/szubov/android_hw_61/MainActivity.java
+++ app/src/main/java/com/szubov/android_hw_61/MainActivity.java
@@ -1,29 +1,18 @@
 package com.szubov.android_hw_61;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextViewValues;
-    private Button mBtnSeven;
-    private Button mBtnEight;
-    private Button mBtnNine;
-    private Button mBtnFour;
-    private Button mBtnFive;
-    private Button mBtnSix;
-    private Button mBtnOne;
-    private Button mBtnTwo;
-    private Button mBtnThree;
-    private Button mBtnZero;
-    private Button mBtnDot;
+    private ArrayDeque<String> values = new ArrayDeque<>();
 
 
     @Override
@@ -37,87 +26,106 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
         mTextViewValues = findViewById(R.id.textViewValues);
-        mBtnSeven = findViewById(R.id.btnSeven);
-        mBtnEight = findViewById(R.id.btnEight);
-        mBtnNine = findViewById(R.id.btnNine);
-        mBtnFour = findViewById(R.id.btnFour);
-        mBtnFive = findViewById(R.id.btnFive);
-        mBtnSix = findViewById(R.id.btnSix);
-        mBtnOne = findViewById(R.id.btnOne);
-        mBtnTwo = findViewById(R.id.btnTwo);
-        mBtnThree = findViewById(R.id.btnThree);
-        mBtnZero = findViewById(R.id.btnZero);
-        mBtnDot = findViewById(R.id.btnDot);
+        Button mBtnSeven = findViewById(R.id.btnSeven);
+        Button mBtnEight = findViewById(R.id.btnEight);
+        Button mBtnNine = findViewById(R.id.btnNine);
+        Button mBtnFour = findViewById(R.id.btnFour);
+        Button mBtnFive = findViewById(R.id.btnFive);
+        Button mBtnSix = findViewById(R.id.btnSix);
+        Button mBtnOne = findViewById(R.id.btnOne);
+        Button mBtnTwo = findViewById(R.id.btnTwo);
+        Button mBtnThree = findViewById(R.id.btnThree);
+        Button mBtnZero = findViewById(R.id.btnZero);
+        Button mBtnDot = findViewById(R.id.btnDot);
 
 
         mBtnSeven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                mTextViewValues.setText(new valueString.add(getText(R.string.btn_seven).toString()));
+                if (values.isEmpty() || !mTextViewValues.getText().toString().matches("^(0)$")) {
+                    addValueToTextView(getText(R.string.btn_seven).toString());
+                }
             }
         });
 
         mBtnEight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextViewValues.setText(getText(R.string.btn_eight).toString());
+                if (values.isEmpty() || !mTextViewValues.getText().toString().matches("^(0)$")) {
+                    addValueToTextView(getText(R.string.btn_eight).toString());
+                }
             }
         });
 
         mBtnNine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextViewValues.setText(getText(R.string.btn_nine).toString());
+                if (values.isEmpty() || !mTextViewValues.getText().toString().matches("^(0)$")) {
+                    addValueToTextView(getText(R.string.btn_nine).toString());
+                }
             }
         });
 
         mBtnFour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextViewValues.setText(getText(R.string.btn_four).toString());
+                if (values.isEmpty() || !mTextViewValues.getText().toString().matches("^(0)$")) {
+                    addValueToTextView(getText(R.string.btn_four).toString());
+                }
             }
         });
 
         mBtnFive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextViewValues.setText(getText(R.string.btn_five).toString());
+                if (values.isEmpty() || !mTextViewValues.getText().toString().matches("^(0)$")) {
+                    addValueToTextView(getText(R.string.btn_five).toString());
+                }
             }
         });
 
         mBtnSix.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextViewValues.setText(getText(R.string.btn_six).toString());
+                if (values.isEmpty() || !mTextViewValues.getText().toString().matches("^(0)$")) {
+                    addValueToTextView(getText(R.string.btn_six).toString());
+                }
             }
         });
 
         mBtnOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextViewValues.setText(getText(R.string.btn_one).toString());
+                if (values.isEmpty() || !mTextViewValues.getText().toString().matches("^(0)$")) {
+                    addValueToTextView(getText(R.string.btn_one).toString());
+                }
             }
         });
 
         mBtnTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextViewValues.setText(getText(R.string.btn_two).toString());
+                if (values.isEmpty() || !mTextViewValues.getText().toString().matches("^(0)$")) {
+                    addValueToTextView(getText(R.string.btn_two).toString());
+                }
             }
         });
 
         mBtnThree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextViewValues.setText(getText(R.string.btn_three).toString());
+                if (values.isEmpty() || !mTextViewValues.getText().toString().matches("^(0)$")) {
+                    addValueToTextView(getText(R.string.btn_three).toString());
+                }
             }
         });
 
         mBtnZero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextViewValues.setText(getText(R.string.btn_zero).toString());
+                if (values.isEmpty() || !mTextViewValues.getText().toString().matches("^(0)$")) {
+                    addValueToTextView(getText(R.string.btn_zero).toString());
+                }
             }
         });
 
@@ -125,9 +133,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (!mTextViewValues.getText().toString().contains(getText(R.string.btn_dot).toString())) {
-                    mTextViewValues.setText(getText(R.string.btn_dot).toString());
+                    if (!values.isEmpty()) {
+                        addValueToTextView(getText(R.string.btn_dot).toString());
+                    }
                 }
             }
         });
+    }
+
+    private String editValuesToString(ArrayDeque<String> values) {
+        StringBuilder sb = new StringBuilder();
+        for (String value : values) {
+            sb.append(value);
+        }
+        return sb.toString();
+    }
+
+    private void addValueToTextView(String string) {
+        if (values.size() <= 30) {
+            values.add(string);
+            mTextViewValues.setText(editValuesToString(values));
+        } else {
+            Toast.makeText(MainActivity.this,
+                    getText(R.string.too_many_characters).toString(), Toast.LENGTH_LONG).show();
+        }
     }
 }
